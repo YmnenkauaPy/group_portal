8268cbcf0c7086fa6f384b8087f74b0a8fdeccf4
arkady300iq <arkadijrivior@gmail.com>
2024-10-05 14:42:22 +0300

    added requirements

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("details", "profile_picture", )
+        fields = UserCreationForm.Meta.fields + ("details", "profile_picture" )
         widgets = {
             'profile_picture': forms.FileInput(),
         }
